57bf601928366d2af711c345639362786605cb68
Irwin Hackshaw <64607426+Irwinhackshaw@users.noreply.github.com>
2022-02-21 21:16:16 -0400

    error on enviromnent fixing

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ import os
 
 # @login_required
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
+print(os.environ)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "sqlite:///blog.db")
